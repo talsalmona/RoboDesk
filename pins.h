1bd06371614a14f62a8089231eeba9921977add6
Tal Salmona <222969+talsalmona@users.noreply.github.com>
2019-03-04 09:27:33 +0200

    Update pins.h

--- pins.h
+++ pins.h
@@ -1,25 +1,16 @@
 // Input
-#define MOD_TX  2 // Must use a an interrupt pin
+#define MOD_TX  2 // TxD - Must use a an interrupt pin
+#define MOD_HS1 4 
+#define MOD_HS2 3 
+
+//optional
+#define BUZZER 5
+#define STATUS_LED 13 // This maps to the default onboard led for Arduino boards 
 
 //unused? TX pin for LogicData class.
 #define INTF_TX 10 // not used but needed for LogicData initialization - put any pin here
 
-// Input/Output (v1.0 cable pins are arranged in this order)
-#define MOD_HS1 4 
-#define MOD_HS2 3 
-
-#define STATUS_LED 13 
-
-#define BUZZER 5
-
-// red - vin
-// brown - ground
-// blue - tx
-// yellow - HS2
-// orange - HS1
-
 // 7 pin din socket
-
 //1: RxD
 //2: HS3
 //3: HS1 up
